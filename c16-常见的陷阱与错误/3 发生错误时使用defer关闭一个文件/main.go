d75f90686e6f2ae592302f88fc94e82dd02f6817
pengjiaxin1 <pengjiaxin1@xiaomi.com>
2021-01-25 22:40:28 +0800

    fix

--- c16-常见的陷阱与错误/3 发生错误时使用defer关闭一个文件/main.go
+++ c16-常见的陷阱与错误/3 发生错误时使用defer关闭一个文件/main.go
@@ -6,10 +6,20 @@ func main() {
 	//	if f, err = os.Open(file); err != nil {
 	//		return
 	//	}
-	//	// 这是错误的方式，当循环结束时文件没有关闭
+	//	// 这是错误的方式，当循环结束时文件没有关闭，因为defer是在函数终止时执行
 	//	//defer f.close()
 	//	// 对文件进行操作
 	//	//f.Process(data)
 	//}
 
+	// 更好的做法：
+	//for _, file := range files {
+	//	if f, err = os.Open(file); err != nil {
+	//		return
+	//	}
+	//	// 对文件进行操作
+	//	f.Process(data)
+	//	// 关闭文件
+	//	f.Close()
+	//}
 }
