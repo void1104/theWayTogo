3823e1aadf6cdb40c69df5d56652d3a16872d329
pengjiaxin1 <pengjiaxin1@xiaomi.com>
2021-01-25 18:35:33 +0800

    fix

--- c15-网络，模板与网页应用/11 与websocket通信/server.go
+++ c15-网络，模板与网页应用/11 与websocket通信/server.go
@@ -1,12 +1,12 @@
 package main
 
-import websocket "code.google.com/p/go/websocket"
-/**
-与http协议相反，websocket是客户端与服务器之间的对话，
-建立的基于单个持久连接的协议。
-然而在其他方面，其功能几乎与http相同
- */
-
-func server(ws *websockt.Conn){
-
-}
+//import websocket "code.google.com/p/go/websocket"
+///**
+//与http协议相反，websocket是客户端与服务器之间的对话，
+//建立的基于单个持久连接的协议。
+//然而在其他方面，其功能几乎与http相同
+// */
+//
+//func server(ws *websockt.Conn){
+//
+//}
